42f57be18559277216983e4e3f1f86e7126b6db9
seonbo.shim <seonbo.shim@SeonBoShim.hwk.net>
2017-03-17 17:17:21 +0900

    a

--- src/main/java/org/chiwooplatform/cloud/sample/rest/CustomerController.java
+++ src/main/java/org/chiwooplatform/cloud/sample/rest/CustomerController.java
@@ -1,5 +1,7 @@
 package org.chiwooplatform.cloud.sample.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomerController {
 
+
+    private static Logger logger = LoggerFactory.getLogger(CustomerController.class);
+    
 	@Value("${greeting:Hi~~}")
 	private String greeting;
 
 	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
 	public String greeting() {
+	    logger.info("greeting");
 		return greeting;
 	}
 }
